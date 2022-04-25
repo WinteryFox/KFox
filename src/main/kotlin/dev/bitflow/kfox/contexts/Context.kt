a0b278d0ae6b8433f686efae645ef4adb153ab8b
AmyFoxie <dragonhel5@gmail.com>
2022-04-25 19:14:38 +0200

    allow registering of components outside of context

--- src/main/kotlin/dev/bitflow/kfox/contexts/Context.kt
+++ src/main/kotlin/dev/bitflow/kfox/contexts/Context.kt
@@ -6,23 +6,38 @@ import dev.kord.rest.builder.component.ButtonBuilder
 import dev.kord.rest.builder.component.SelectMenuBuilder
 import dev.kord.rest.builder.interaction.ModalBuilder
 
+@Suppress("unused")
+suspend inline fun ButtonBuilder.InteractionButtonBuilder.register(registry: ComponentRegistry, callbackId: String) {
+    registry.save(customId, callbackId)
+}
+
+@Suppress("unused")
+suspend inline fun SelectMenuBuilder.register(registry: ComponentRegistry, callbackId: String) {
+    registry.save(customId, callbackId)
+}
+
+@Suppress("unused")
+suspend inline fun ModalBuilder.register(registry: ComponentRegistry, callbackId: String) {
+    registry.save(customId, callbackId)
+}
+
 sealed class Context(
     val client: Kord,
-    val registry: ComponentRegistry
+    private val registry: ComponentRegistry
 ) {
     @Suppress("unused")
     suspend fun ButtonBuilder.InteractionButtonBuilder.register(callbackId: String) {
-        registry.save(customId, callbackId)
+        register(registry, callbackId)
     }
 
     @Suppress("unused")
     suspend fun SelectMenuBuilder.register(callbackId: String) {
-        registry.save(customId, callbackId)
+        register(registry, callbackId)
     }
 
     @Suppress("unused")
     suspend fun ModalBuilder.register(callbackId: String) {
-        registry.save(customId, callbackId)
+        register(registry, callbackId)
     }
 }
 
