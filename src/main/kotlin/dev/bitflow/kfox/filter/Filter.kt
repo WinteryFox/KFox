88d825513355cd1a9f1fb59c999fd80e647106a9
ByteAlex <abecker@bitflow.dev>
2022-07-06 23:34:36 +0200

    Missed these

--- src/main/kotlin/dev/bitflow/kfox/filter/Filter.kt
+++ src/main/kotlin/dev/bitflow/kfox/filter/Filter.kt
@@ -1,7 +1,8 @@
 package dev.bitflow.kfox.filter
 
-import dev.bitflow.kfox.context.*
+import dev.bitflow.kfox.AsKordEvent
+import dev.bitflow.kfox.context.Context
 
 interface Filter {
-    suspend fun doFilter(context: Context): Boolean
+    suspend fun <T, E : AsKordEvent<T>> doFilter(context: Context<T, E>): Boolean
 }
--- src/main/kotlin/dev/bitflow/kfox/filter/GuildFilter.kt
+++ src/main/kotlin/dev/bitflow/kfox/filter/GuildFilter.kt
@@ -1,8 +1,9 @@
 package dev.bitflow.kfox.filter
 
+import dev.bitflow.kfox.AsKordEvent
 import dev.bitflow.kfox.context.Context
 
 object GuildFilter : Filter {
-    override suspend fun doFilter(context: Context): Boolean =
+    override suspend fun <T, E : AsKordEvent<T>> doFilter(context: Context<T, E>): Boolean =
         context.event.interaction.data.guildId.value != null
 }
