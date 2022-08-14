ce8768b8e1a4c2f130832f3266b7aee9363c0db3
AmyFoxie <dragonhel5@gmail.com>
2022-08-14 23:27:12 +0200

    fix test bot

--- src/test/kotlin/dev/bitflow/kfox/TestBot.kt
+++ src/test/kotlin/dev/bitflow/kfox/TestBot.kt
@@ -19,60 +19,10 @@ const val MODAL_CALLBACK = "modal"
 @Module("help")
 @Filter(GuildFilter::class)
 suspend fun help(
-    context: EphemeralChatCommandContext
+    context: EphemeralChatCommandContext<*>
 ) = with(context) {
     response.createEphemeralFollowup {
         content = getUserString("content")
-    }
-}
-
-@Modal(MODAL_CALLBACK)
-suspend fun modal(
-    context: PublicModalContext,
-    @ModalValue("poem")
-    modalValue: String,
-    @ModalValue("name")
-    what: String
-) = with(context) {
-    response.createPublicFollowup {
-        content = "Your beautiful poem reads; $modalValue and your name is $what"
-    }
-}
-
-@Command("root-name", "root-desc", guild = 809278232100077629L)
-suspend fun root() {
-}
-
-@Command("sub-name", "sub-desc", guild = 809278232100077629L)
-@SubCommand("root-name")
-@Group("group-name", "group-desc")
-suspend fun sub(context: ChatCommandContext) = with(context) {
-    event.interaction.respondEphemeral { content = "This is a sub-command" }
-}
-
-@Command("parrot-name", "parrot-desc", guild = 809278232100077629L)
-suspend fun testCommand(
-    context: ChatCommandContext,
-    @Parameter("content-name", "content-desc")
-    @Choices(["mlem","i'm trying to do something useful"])
-    value: String
-) {
-    with(context) {
-        event.interaction.modal(getUserString("name"), "aaa") {
-            actionRow {
-                textInput(TextInputStyle.Short, "poem", getUserString("poetry")) {
-                    placeholder = getUserString("poetry-placeholder")
-                }
-            }
-
-            actionRow {
-                textInput(TextInputStyle.Short, "name", getUserString("name")) {
-                    placeholder = getUserString("name-placeholder")
-                }
-            }
-
-            register(MODAL_CALLBACK)
-        }
     }
 }
 
@@ -88,7 +38,7 @@ class TestBot {
     @Test
     fun testBot(): Unit = runBlocking {
         val translationProvider =
-            ResourceBundleTranslationProvider("test", Locale.ENGLISH_UNITED_STATES)
+            ResourceBundleTranslationProvider(defaultModule = "test", defaultLocale = Locale.ENGLISH_UNITED_STATES)
         val kfox = client.KFox("dev.bitflow.kfox", translationProvider)
         kfox.listen()
         client.login()
